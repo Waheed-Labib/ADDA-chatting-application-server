04d43b39349a0975e3aa16532eac60b0cf246d8f
Waheed-Labib <labibwahid28@gmail.com>
2023-08-11 11:20:39 +0600

    updated index.js

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
 const app = express()
 const port = process.env.PORT || 5000;
 
 app.use(cors())
 
+const users = require('./fake data/users.json')
+const groups = require('./fake data/groups.json')
+const messages = require('./fake data/messages.json')
+
 app.get('/', (req, res) => {
     res.send('ADDA Server running')
 })
 
+app.get('/users', (req, res) => {
+    res.send(users)
+})
 
+app.get('/groups', (req, res) => {
+    res.send(groups)
+})
+
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
 
 app.listen(port, () => {
     console.log('ADDA Server running on port : ', port)
